cf9f6d473cfa48b9037573444fbbc7893dffe6d0
mbilalabid800 <mbilalabid800@gmail.com>
2025-01-21 17:57:31 +0500

    bilal 21-1

--- lib/screens/auth/forget_password.dart
+++ lib/screens/auth/forget_password.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: file_names, use_build_context_synchronously, deprecated_member_use, avoid_print
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nfc_app/constants/appColors.dart';
 import 'package:nfc_app/provider/authenticate_provider.dart';
+import 'package:nfc_app/provider/user_info_form_state_provider.dart';
 import 'package:nfc_app/responsive/device_dimensions.dart';
 import 'package:nfc_app/shared/common_widgets/my_button.dart';
+import 'package:nfc_app/shared/common_widgets/my_textfield.dart';
 
 import 'package:provider/provider.dart';
 
@@ -22,8 +23,8 @@ class _ForgetPasswordState extends State<ForgetPassword> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Consumer<AuthenticateProvider>(
-          builder: (context, authProvider, child) => Stack(
+        body: Consumer2<AuthenticateProvider, UserInfoFormStateProvider>(
+          builder: (context, authProvider, userProvider, child) => Stack(
             children: [
               SizedBox(
                 height: DeviceDimensions.screenHeight(context) * 0.70,
@@ -42,27 +43,26 @@ class _ForgetPasswordState extends State<ForgetPassword> {
                     borderRadius: BorderRadius.circular(30),
                     color: Colors.white,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 34),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                            height:
-                                DeviceDimensions.screenHeight(context) * 0.015),
-                        Container(
-                          width: DeviceDimensions.screenWidth(context) * 0.16,
+                  child: Column(
+                    children: [
+                      SizedBox(
                           height:
-                              DeviceDimensions.screenHeight(context) * 0.007,
-                          decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
+                              DeviceDimensions.screenHeight(context) * 0.015),
+                      Container(
+                        width: DeviceDimensions.screenWidth(context) * 0.16,
+                        height: DeviceDimensions.screenHeight(context) * 0.007,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(12.0),
                         ),
-                        SizedBox(
-                            height:
-                                DeviceDimensions.screenHeight(context) * 0.030),
-                        Align(
-                          alignment: Alignment.centerLeft,
+                      ),
+                      SizedBox(
+                          height:
+                              DeviceDimensions.screenHeight(context) * 0.030),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 21),
                           child: Text(
                             "Forgot password",
                             style: TextStyle(
@@ -73,10 +73,13 @@ class _ForgetPasswordState extends State<ForgetPassword> {
                                 fontFamily: 'Barlow-Bold'),
                           ),
                         ),
-                        SizedBox(
-                            height:
-                                DeviceDimensions.screenHeight(context) * 0.015),
-                        Text(
+                      ),
+                      SizedBox(
+                          height:
+                              DeviceDimensions.screenHeight(context) * 0.015),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 21),
+                        child: Text(
                           "In order to reset password ,please provide us your email. We will send you an verification code momentarily.",
                           style: TextStyle(
                               fontSize:
@@ -85,72 +88,87 @@ class _ForgetPasswordState extends State<ForgetPassword> {
                               fontFamily: 'Barlow-Regular',
                               color: AppColors.appBlueColor),
                         ),
-                        SizedBox(
-                            height:
-                                DeviceDimensions.screenHeight(context) * 0.050),
-                        Form(
-                          key: authProvider.forgetPassowrdFormKey,
-                          child: TextFormField(
+                      ),
+                      SizedBox(
+                          height:
+                              DeviceDimensions.screenHeight(context) * 0.050),
+                      Form(
+                        key: authProvider.forgetPassowrdFormKey,
+                        child: MyTextfield(
                             controller:
                                 authProvider.forgetPasswordEmailController,
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                              hintText: "Email",
-                              hintStyle: const TextStyle(
-                                color: Color(0xFFA9A9A9),
-                                fontFamily: 'Barlow-Regular',
-                                fontWeight: FontWeight.w500,
-                              ),
-                              prefixIcon: Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 12.0, bottom: 12, left: 20, right: 10),
-                                child: SvgPicture.asset(
-                                  "assets/icons/email.svg",
-                                  height: 17,
-                                  width: 17,
-                                  fit: BoxFit.contain,
-                                ),
-                              ),
-                              filled: true,
-                              fillColor: Colors.white,
-                              contentPadding: const EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 10),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                                borderSide:
-                                    const BorderSide(color: Color(0xFFA9A9A9)),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                                borderSide:
-                                    const BorderSide(color: Color(0xFFA9A9A9)),
-                              ),
-                            ),
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Please enter your email';
-                              }
-                              final emailPattern =
-                                  RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-                              if (!emailPattern.hasMatch(value)) {
-                                return 'Please enter a valid email address';
-                              }
-                              return null;
+                            hintText: 'Email',
+                            iconPath: 'assets/icons/email.svg',
+                            errorText: userProvider.emailError,
+                            onChanged: (value) {
+                              userProvider.validateEmail(value);
                             },
-                          ),
-                        ),
-                        SizedBox(
-                            height:
-                                DeviceDimensions.screenHeight(context) * 0.065),
-                        MyButton(
-                          text: "Forget Password",
-                          onPressed: () {
-                            authProvider.resetPassword(context);
-                          },
-                          width: DeviceDimensions.screenWidth(context) * 0.82,
-                        ),
-                      ],
-                    ),
+                            validator: (value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Please enter an email address';
+                              }
+                              return userProvider.emailError;
+                            }),
+                        // child: TextFormField(
+                        //   controller:
+                        //       authProvider.forgetPasswordEmailController,
+                        //   keyboardType: TextInputType.emailAddress,
+                        //   decoration: InputDecoration(
+                        //     hintText: "Email",
+                        //     hintStyle: const TextStyle(
+                        //       color: Color(0xFFA9A9A9),
+                        //       fontFamily: 'Barlow-Regular',
+                        //       fontWeight: FontWeight.w500,
+                        //     ),
+                        //     prefixIcon: Padding(
+                        //       padding: const EdgeInsets.only(
+                        //           top: 12.0, bottom: 12, left: 20, right: 10),
+                        //       child: SvgPicture.asset(
+                        //         "assets/icons/email.svg",
+                        //         height: 17,
+                        //         width: 17,
+                        //         fit: BoxFit.contain,
+                        //       ),
+                        //     ),
+                        //     filled: true,
+                        //     fillColor: Colors.white,
+                        //     contentPadding: const EdgeInsets.symmetric(
+                        //         vertical: 10, horizontal: 10),
+                        //     enabledBorder: OutlineInputBorder(
+                        //       borderRadius: BorderRadius.circular(15),
+                        //       borderSide:
+                        //           const BorderSide(color: Color(0xFFA9A9A9)),
+                        //     ),
+                        //     focusedBorder: OutlineInputBorder(
+                        //       borderRadius: BorderRadius.circular(15),
+                        //       borderSide:
+                        //           const BorderSide(color: Color(0xFFA9A9A9)),
+                        //     ),
+                        //   ),
+                        //   validator: (value) {
+                        //     if (value!.isEmpty) {
+                        //       return 'Please enter your email';
+                        //     }
+                        //     final emailPattern =
+                        //         RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                        //     if (!emailPattern.hasMatch(value)) {
+                        //       return 'Please enter a valid email address';
+                        //     }
+                        //     return null;
+                        //   },
+                        // ),
+                      ),
+                      SizedBox(
+                          height:
+                              DeviceDimensions.screenHeight(context) * 0.065),
+                      MyButton(
+                        text: "Forget Password",
+                        onPressed: () {
+                          authProvider.resetPassword(context);
+                        },
+                        width: DeviceDimensions.screenWidth(context) * 0.82,
+                      ),
+                    ],
                   ),
                 ),
               ),
