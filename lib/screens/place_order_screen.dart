1936e618e64de9f2e32825dc93fad7c802894f88
ayeshaamjad01 <ashamjad007@gmail.com>
2025-02-19 18:33:18 +0500

    ayesha 19-2

--- lib/screens/place_order_screen.dart
+++ lib/screens/place_order_screen.dart
@@ -5,6 +5,7 @@ import 'package:nfc_app/constants/appColors.dart';
 import 'package:nfc_app/models/card_details_model.dart';
 import 'package:nfc_app/models/shipping_address_model.dart';
 import 'package:nfc_app/provider/employee_provider.dart';
+import 'package:nfc_app/provider/order_provider.dart';
 import 'package:nfc_app/provider/shipping_address_provider.dart';
 import 'package:nfc_app/provider/user_info_form_state_provider.dart';
 import 'package:nfc_app/responsive/device_dimensions.dart';
@@ -149,12 +150,6 @@ class _PlaceOrderScreenState extends State<PlaceOrderScreen> {
                                             0.025),
                                 Column(
                                   children: [
-                                    // deliveryMethod(context,
-                                    //     'Pick from nearby machine', provider),
-                                    // SizedBox(
-                                    //     height:
-                                    //         DeviceDimensions.screenHeight(context) *
-                                    //             0.015),
                                     Padding(
                                       padding:
                                           const EdgeInsets.only(right: 15.0),
