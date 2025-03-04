e85f9022a66ffd57afc7e1c24ba9da6fbbcf0937
ayeshaamjad01 <ashamjad007@gmail.com>
2025-03-04 11:06:26 +0500

    ayesha 3-3

--- lib/widgets/confirm_order.dart
+++ lib/widgets/confirm_order.dart
@@ -12,6 +12,7 @@ import 'package:nfc_app/provider/user_info_form_state_provider.dart';
 import 'package:nfc_app/responsive/device_dimensions.dart';
 import 'package:nfc_app/shared/common_widgets/custom_loader_widget.dart';
 import 'package:nfc_app/shared/common_widgets/custom_snackbar_widget.dart';
+import 'package:nfc_app/shared/utils/no_back_button_observer.dart';
 import 'package:nfc_app/widgets/payment_successful.dart';
 import 'package:provider/provider.dart';
 
